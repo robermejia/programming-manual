0581ef4110bf659f247ea3dec1494019afa511c0
Roberto Mejía <59416763+robermejia@users.noreply.github.com>
2026-02-10 15:20:00 -0500

    Actualizar vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -6,6 +7,17 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 5173
+  },
+  build: {
+    chunkSizeWarningLimit: 1000, // sube el límite (1 MB)
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'
+          }
+        }
+      }
+    }
   }
 })
-
