e78cc199deaf8928ce0c394d886d67dcc23b8c09
Roberto Mejía <59416763+robermejia@users.noreply.github.com>
2026-02-10 15:26:54 -0500

    Actualizando2 vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,15 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: './',
   plugins: [react()],
   server: {
     host: '0.0.0.0',
     port: 5173
   },
   build: {
-    chunkSizeWarningLimit: 1000, // sube el límite (1 MB)
+    chunkSizeWarningLimit: 1000,
     rollupOptions: {
       output: {
         manualChunks(id) {
